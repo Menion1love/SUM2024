30d139d860ec34d469f79d4aa1bb7cd4da524352
Худяков Тимофей (TH4) <cgsg198@ad.pml30.ru>
2024-06-12 19:35:57 +0300

    School commit 12.06.2024

--- 04_server/client/index.js
+++ 04_server/client/index.js
@@ -1,22 +1,33 @@
-import {f} from "./1.js"
+let usName = "";
+let html = '<table id="mytable" class="mytable"><tbody><td class="mytd">'
 
-console.log("Client js load.")
+let socket = new WebSocket("ws://localhost:8000")
+  
+socket.onopen = (event) => {
+  document.getElementById('msg_table').innerHTML = html + event.data + '</td></tbody></table>';
+} 
+socket.onmessage = (event) => {
+  document.getElementById('msg_table').innerHTML = html + event.data + '</td></tbody></table>';
+} 
 
-function initCom() {
-  let socket = new WebSocket("ws://localhost:8000")
-
-  socket.onopen = (event) => {
-    console.log("Socket open")
-    socket.send("les gooo")
+document.getElementById("name").addEventListener("keydown", function(e) {
+  if (e.which === 13 || e.which === 9)
+  {
+    let v = document.getElementById("name").value;
+    if (v != "" && v.indexOf(' ') == -1)
+      usName = v
+  }
+})
+  
+document.getElementById("message").addEventListener("keydown", function(e) {
+  if (e.which === 13)
+  {
+    let v = document.getElementById("message").value
+    if (v != "" && usName != "")
+      {
+        let m = '<br>' + `${usName}: ${v}` + '<br/>'
+        document.getElementById("message").value = ""
+        socket.send(m.toString())
+    } 
   } 
-
-  socket.onmessage = (event) => {
-    console.log(`Message ${event.data}`)
-  }
-}
-initCom()
-function checkForEnter(e) {
-    if (e.keyCode == 13) { 
-      document.querySelector(".block .button").click();
-    }
-  }
+})
--- 04_server/main.js
+++ 04_server/main.js
@@ -1,56 +1,51 @@
 import http from "node:http";
-import fs from "node:fs/promises";
-import process from "node:process";
+import fs from "node:fs"
+import fsw from "node:fs/promises"
 import express from "express";
 import {WebSocketServer} from "ws";
 
-let cnt = 0;
-
-// const requestListener = async (req, res) => {
-//   if (req.url == "/") {
-//     const contents = await fs.readFile(process.cwd() + "/client/index.html")
-//     res.setHeader("Content-Type", "text/html");
-//     res.writeHead(200);
-//     res.end(contents);
-//   } else {
-//     if (req.url.endsWith(".js")) {
-//       const contents = await fs.readFile(process.cwd() + "/client" + req.url)
-//       res.setHeader("Content-Type", "text/javascript");
-//       res.writeHead(200);
-//       res.end(contents);
-
-//     }
-//   }
-// };
+const hist = [];
+let cnt = 0
 const app = express();
+
+const clients = [];
 
 app.get("/", (req, res, next) => {
   cnt++;
   console.log(cnt);
+  console.log(clients)
   next();
-
 })
 
 app.use(express.static("client"))
 
 const server = http.createServer(app);
 
-const wss = new WebSocketServer({ server }); 
+const wss = new WebSocketServer({ server });   
 
 wss.on("connection", (ws) => {
+  clients.push(ws)
+  for (let c in clients)
+    clients[c].send(hist.join(''))
   ws.on("message", (message) => {
-    console.log(message.toString());
-    ws.send(`Hello, ${message}`);
+    let newm = message.toString()
+    hist.push(newm)
+    console.log(hist)
+    console.log(hist.join(''))
+    fsw.writeFile("./client/chat.txt", hist.join(''))
+    for (let c in clients)
+      clients[c].send(hist.join(''))
   });
 
-  ws.send("maan");
+  ws.on("close", () => {
+    clients.pop(ws);
+  })
 })
 
 const host = "localhost";
 const port = 8000;
 
 server.listen(port, host, () => {
+  hist.push(fs.readFileSync('./client/chat.txt', 'utf8'))
   console.log(`Server started on http://${host}:${port}`);
 });
-
-
